7baa2b3b44d51b177a78317b8a4468549bbefaa3
Sidney Blankenship <sidneyblankenship1@gmail.com>
2026-03-21 19:53:57 -0400

    added reporting into a json file and cleaned up terminal output

---
+++ json_report.py
@@ -0,0 +1,10 @@
+# json_report.py
+
+import json
+
+
+def write_json_report(page_data, filename="report.json"):
+    unsorted_data = []
+    pages = sorted(page_data.values(), key=lambda p: p["url"])
+    with open(filename, "w", encoding="utf=8") as f:
+        json.dump(pages, f, indent=2)
