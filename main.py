95d4c663c349796a4a4394c822469b17828aeeda
Sidney Blankenship <sidneyblankenship1@gmail.com>
2026-03-21 00:33:22 -0400

    feat: initiated cli for two args

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+# main.py
+
+import sys
+
+
 def main():
-    print("Hello from intro-web-scraper!")
+    if len(sys.argv) < 2:
+        print("no website provided")
+        sys.exit(1)
+    elif len(sys.argv) > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+    else:
+        BASE_URL = sys.argv[1]
+        print(f"starting crawl of: {BASE_URL}")
 
 
 if __name__ == "__main__":
